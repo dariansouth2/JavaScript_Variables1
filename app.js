cda77da835ecd266ef90bdd962ec4243ac4eedbb
Darian Southerland <darsouthland@gmail.com>
2022-10-19 17:57:22 -0500

    Upd1

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Darian";
+let lastName = "Southerland";
+var age = 19;
+
 
 // Exercise 2
+lastName = "Wayne";
+age = 45;
 
 // Exercise 3
+let language="JavaScript";
+let createdYear= 1995;
+let inCaseSenitive= true;
+
+//Exercise 4
+let price= 19.99;
+let isOnSale= false;
+let salePercentage=15;
+let stock=0;
+let inStock= false;
+let selectedSize= "M";
+
+//Exercise 5
+let title= "Name of the Wind";
+let author= "Patrick Rothfuss";
+let pageCount= 722;
+let bookmark= 456;
+let hasRead= true;
